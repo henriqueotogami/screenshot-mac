7bf6c83826cfd6a01e2c9cbcff5ba047abc8dd68
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2024-05-29 16:01:12 -0300

    Classe de viewModel movida para pasta de viewmodel

---
+++ view/settings/KeyboardShortcutsView.swift
@@ -0,0 +1,8 @@
+//
+//  KeyboardShortcutsView.swift
+//  screenshot-mac
+//
+//  Created by Henrique Matheus Alves Pereira on 29/05/24.
+//
+
+import Foundation
---
+++ view/settings/SettingsView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsView.swift
+//  screenshot-mac
+//
+//  Created by Henrique Matheus Alves Pereira on 29/05/24.
+//
+
+import Foundation
